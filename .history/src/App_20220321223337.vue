2df2be466fdfcaf401791f25edf6540b4f517db8
yulingying <2905719283@qq.com>
2022-03-21 22:42:23 +0800

    项目初始化-调整初始目录结构

--- .history/src/App_20220321223337.vue
+++ .history/src/App_20220321223337.vue
@@ -2,15 +2,8 @@
   <div id="app">
     <!-- 路由出口 -->
     <router-view/>
-    <h1>黑马头条</h1>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
 
 <style lang="less">
 </style>
--- .history/src/router/index_20220321223439.js
+++ .history/src/router/index_20220321223439.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-// 路由表
 const routes = []
 
 const router = new VueRouter({
